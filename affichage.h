1cf7af33d9b20d98dba46065da875557e9c2591f
Céline Roger <roger.celine93@gmail.com>
2022-05-24 11:11:46 +0200

    ajout fichiers + headers

---
+++ affichage.h
@@ -0,0 +1,15 @@
+/*
+ ---------------------------------------------------------------------------------------------------------------------
+ Nom du fichier : main.c
+ Auteur(s)      :
+ Date creation  :
+ Description    :
+ Remarque(s)    :
+ Compilateur    : Mingw-w64 gcc 8.1.0
+ ---------------------------------------------------------------------------------------------------------------------
+*/
+
+#ifndef PRG2_L2_AFFICHAGE_H
+#define PRG2_L2_AFFICHAGE_H
+
+#endif //PRG2_L2_AFFICHAGE_H
