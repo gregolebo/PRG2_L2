32e7dfbaa181b1a3c1cb418aa71442f1b396ddc6
Céline Roger <roger.celine93@gmail.com>
2022-05-24 15:58:29 +0200

    déclaration constantes taxe

--- taxes.h
+++ taxes.h
@@ -15,4 +15,16 @@
 // définir les taxes avec des constantes et non define
 typedef double CHF;
 
+// Taxe de base
+const CHF TAXE_BASE_CAMIONNETTE = 700;
+const CHF TAXE_BASE_VOITURE = 400;
+
+// Taxe specifique
+const CHF COEFF_TAXE_VOL_TRANSP_CAM = 10;
+const CHF TAXE_CRITERES1_STAND = 50; // TODO nom plus cohérent à trouver
+const CHF COEFF_TAXE_CYLINDREE_STAND = .05;
+const CHF TAXE_PUISSANCE_HDG = 200;
+const CHF TAXE_POIDS_BASE_HDG = 300;
+const CHF COEFF_TAXE_POIDS_HDG = 20;
+
 #endif //PRG2_L2_TAXES_H
