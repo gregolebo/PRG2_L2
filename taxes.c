309f464ae56f16efe7702c2a6802f05da50ed697
pablo-urizar <jose.urizar@heig-vd.ch>
2022-05-30 18:26:22 +0200

    Calcul taxe annuelle véhicule

--- taxes.c
+++ taxes.c
@@ -10,3 +10,47 @@
 */
 
 #include "taxes.h"
+//#include "voitures.h"
+
+CHF taxeAnnuelle(const Vehicule* vehicule) {
+   CHF taxe = 0.f;
+
+   switch(vehicule->typeVehicule) {
+      case CAMIONETTE:
+         taxe += (TAXE_BASE_CAMIONNETTE + COEFF_TAXE_VOL_TRANSP_CAM *
+            vehicule->categorieVehicule.camionnette.volumeTransport);
+         break;
+
+      case VOITURE:
+         taxe += TAXE_BASE_VOITURE;
+
+         // Inclure voitures.h pour utiliser "STANDARD" au lieu de "0" dans le if ?
+         //if (vehicule->categorieVehicule.voiture.typeVoiture == STANDARD)
+
+         // Voiture Standard
+         if (vehicule->categorieVehicule.voiture.typeVoiture == 0) {
+            if (vehicule->categorieVehicule.voiture.specVoiture.voitureStandard.cylindree < SEUIL_CYLINDREE) {
+               if (vehicule->categorieVehicule.voiture.specVoiture.voitureStandard.rejetCo2 >= SEUIL_REJET_CO2)
+                  taxe += TAXE_CRITERES1_STAND;
+
+            } else
+               taxe += (COEFF_TAXE_CYLINDREE_STAND *
+                  vehicule->categorieVehicule.voiture.specVoiture.voitureStandard.cylindree);
+
+         } else {
+            // Voiture haut de gamme
+            if (vehicule->categorieVehicule.voiture.specVoiture.voitureHautGamme.puissance <= SEUIL_PUISSANCE)
+               taxe += TAXE_PUISSANCE_HDG;
+            else
+               taxe += (TAXE_POIDS_BASE_HDG + (COEFF_TAXE_POIDS_HDG * vehicule->categorieVehicule.voiture.poids));
+
+         }
+         break;
+
+      default:
+         break;
+
+   }
+
+   return taxe;
+}
--- taxes.h
+++ taxes.h
@@ -12,23 +12,29 @@
 #ifndef PRG2_L2_TAXES_H
 #define PRG2_L2_TAXES_H
 #include <inttypes.h>
+#include "voitures.h"
 
 // Taxes de base
 #define TAXE_BASE_CAMIONNETTE 700
 #define TAXE_BASE_VOITURE 400
 
-// Taxe specifique
+// Taxes specifiques
 #define COEFF_TAXE_VOL_TRANSP_CAM 10
 #define TAXE_CRITERES1_STAND 50
-#define COEFF_TAXE_CYLINDREE_STAND 0.5
+#define COEFF_TAXE_CYLINDREE_STAND 0.05
 #define TAXE_PUISSANCE_HDG 200
 #define TAXE_POIDS_BASE_HDG 300
 #define COEFF_TAXE_POIDS_HDG 20
 
 // Seuils de taxation
-#define SEUIL_CYLINDREE 1400
-#define SEUIL_REJET_CO2 130
-#define SEUIL_PUISSANCE 250
+#define SEUIL_CYLINDREE 1400 // [cm3]
+#define SEUIL_REJET_CO2 130 // [g/km]
+#define SEUIL_PUISSANCE 250 // [CV]
+
+typedef double CHF;
+
+// Fonctions de calcul de taxes
+CHF taxeAnnuelle(const Vehicule* vehicule);
 
 /*
 // définir les taxes avec des constantes et non define
